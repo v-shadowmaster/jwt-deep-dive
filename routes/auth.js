987c15d194894417e91bb2c720ce656e47bd4af7
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-02-19 08:56:47 +0530

    fix: correct login route path in auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
-import User from "../models/user.js";
-import { createAccessToken, createRefreshToken } from "../utils/tokens.js";
-import { registerController } from "../contollers/auth.js";
+import { loginController, registerController } from "../contollers/auth.js";
 
 const router = express.Router();
 
-// register
 router.post("/register", registerController);
+router.post("login", loginController);
 
+export default router;
